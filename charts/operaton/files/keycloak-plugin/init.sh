a68c7c56e16fa14e3b533afc6e9159bbea652963
Eisenack, Frank <278027713+eopterix@users.noreply.github.com>
2026-09-15 12:46:54 +0000

    feat: add config and init script

---
+++ charts/operaton/files/keycloak-plugin/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#PLUGIN_VERSION=2.2.0-M1
+PLUGIN_VERSION=2.1.0
+SRC_URL=https://repo1.maven.org/maven2/org/operaton/bpm/extension/operaton-keycloak-run/${PLUGIN_VERSION}/operaton-keycloak-run-${PLUGIN_VERSION}.jar
+TARGET_FILE=/operaton/configuration/userlib/operaton-keycloak-run-${PLUGIN_VERSION}.jar
+if [ ! -f "$TARGET_FILE" ]; then
+  echo "Keycloak plugin will be added..."
+  curl -o $TARGET_FILE $SRC_URL
+  chmod 644 /operaton/configuration/userlib/operaton-keycloak-run-${PLUGIN_VERSION}.jar
+  echo "Keycloak plugin added."
+else
+  echo "Keycloak plugin already there."
+fi
+echo "Starting Operaton..."
+cd /operaton
+/sbin/tini -- ./operaton.sh
+
